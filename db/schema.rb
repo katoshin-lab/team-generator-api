486f3278a45190781869797825957ba1db6a88a2
katoshin <hatao0000000@outlook.com>
2021-09-02 22:52:41 +0900

    スキーマ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_133756) do
+ActiveRecord::Schema.define(version: 2021_09_02_134749) do
 
   create_table "members", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2021_09_02_133756) do
     t.datetime "invalidated_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_teams_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -49,4 +51,5 @@ ActiveRecord::Schema.define(version: 2021_09_02_133756) do
   add_foreign_key "members", "users"
   add_foreign_key "team_members", "members"
   add_foreign_key "team_members", "teams"
+  add_foreign_key "teams", "users"
 end
