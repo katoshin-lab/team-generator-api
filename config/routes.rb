e652a26096ff3de5ea46bdeb62dbb1684fa28b18
katoshin <hatao0000000@outlook.com>
2021-09-06 23:36:19 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope '/api' do
     namespace :v1 do
-      resources :members, only: %w(index) do
-        
-      end
+
+      resources :members, only: %w(index)
+      resources :teams, only: %w(index)
+
     end
   end
 end
