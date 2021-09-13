17dfab2095389a90b2cc7679ddd30e0d716ccf43
katoshin <hatao0000000@outlook.com>
2021-09-13 23:37:26 +0900

    tag modelにアソシエーションと、タグをつけるためのアソシエーションモジュール作成

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,12 @@
 class Tag < ApplicationRecord
+  belongs_to :taggable, polymorphic: true
+  belongs_to :user
+
+  module TaggableResources
+    extend ActiveSupport::Concern
+    included do
+      has_many :tags, as: :taggable
+    end
+  end
+
 end
