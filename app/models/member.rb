eff0b40165012f571701d75310820a28bfbce1ff
katoshin <hatao0000000@outlook.com>
2021-09-14 22:17:18 +0900

    モジュール化したアソシエーションを使用

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,7 @@
 class Member < ApplicationRecord
+  include Tag::TaggableResources
+
   has_many :team_members
   has_many :teams, through: :team_members
+  belongs_to :user
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ApplicationRecord
+  include Tag::TaggableResources
+
   belongs_to :user
   has_many :team_members
   has_many :members, through: :team_members
